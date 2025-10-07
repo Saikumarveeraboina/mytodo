f2561710e8b65ecef32d9684b06f58bb0d5ea225
Saikumar <saikumar96030433@gmail.com>
2025-10-07 21:48:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,14 @@ const express = require("express")
 const cors = require("cors")
 const mysql = require('mysql2')
 const app = express()
-
 app.use(cors())
 app.use(express.json())
-
-
 const db = mysql.createConnection({
      host : 'localhost',
     user: 'root',
     password: 'saikumar2005',
     database : 'todo'
 })
-
-
 db.connect((err) => {
     if (err) {
         console.log("Error while conneceting to db")
